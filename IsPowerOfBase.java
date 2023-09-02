ddb4f943888b5a8e3d970f5e7584eed61717ca86
Arman Azarnik <armanazarnik@gmail.com>
2023-09-02 17:17:10 +0330

    v 1.0

---
+++ IsPowerOfBase.java
@@ -0,0 +1,36 @@
+/*Check whether if the input number is a power the base number or not*/
+
+import java.util.Scanner;
+
+public class IsPowerOfBase{
+    public static void main(String[] args){
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int number = input.nextInt();
+            System.out.print("Enter a base number: ");
+            int base = input.nextInt();
+            if (IsPowerOfBaseChecker(number, base)){
+                System.out.println(number + " is a power of "+base+".");
+            } else{
+                System.out.println(number + " is not a power of "+base+".");
+            }
+        }
+    }
+
+    public static boolean IsPowerOfBaseChecker(int number, int base){
+        if (number <= 0 || base <=0){
+            return false;
+        }
+        while(number > 1){
+            if (number % base != 0){
+                return false;
+            }
+            number /= base;
+       }
+       return true;
+    }
+}
+
+
+//by Arman Azarnik
